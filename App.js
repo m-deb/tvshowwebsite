cfcbc1d4001563a1f1fab5298bd10df5aa633846
Anand Mishra <51826106+m-deb@users.noreply.github.com>
2021-02-11 15:27:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+//components
+import Navbar from './components/Navbar';
+
+//Pgaes
+import Homepage from './pages/Homepage';
+import Aboutpage from './pages/Aboutpage';
+import Singlepage from './pages/Singlepage';
+
+
+const App = () => {
+    return ( <
+        Router >
+        <
+        Navbar / >
+        <
+        div className = "container" >
+        <
+        Switch >
+        <
+        Route exact path = "/"
+        component = { Homepage }
+        /> <
+        Route exact path = "/about"
+        component = { Aboutpage }
+        /> <
+        Route path = "/singleshow/:id"
+        component = { Singlepage }
+        /> <
+        /Switch> <
+        /div> <
+        /Router>
+    );
+};
+
+export default App;
